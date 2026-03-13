5e6ff41a4a8a40fbcf2826d408d536bc38be055f
10Ag3R <166014914+f0ke13@users.noreply.github.com>
2026-03-13 14:11:24 +0300

    models added
    models = file for importing class "CSMInput"

--- app/algorithms/successive_substitutions_method/schema.py
+++ app/algorithms/successive_substitutions_method/schema.py
@@ -11,19 +11,7 @@ from .parser import (
     validate_constraint_functions_params,
     validate_input_data,
 )
-
-
-@dataclass(frozen=True)
-class CSMInput:
-    variable_cnt: int                        # количество переменных <= 5
-    restriction_cnt: int                     # количество ограничений <= 20
-    targetfunction_cnt: int                  # количество целевых функций <= 3
-    concession_cnt: int                      # количество уступков (количество целевых функций - 1) <= 2
-    targetfunctions: List[List[float]]       # массив целевых функций A[z1[x1,...,xk], z2[...],...]
-    extremumtype_targetfunctions: List[str]  # тип экстремума ('max' или 'min') для каждой функции
-    restrictions: List[List[float]]          # массив ограничений + правая часть со знаком
-    concessions: List[float]                 # массив уступков дельта1, дельта2,...,дельта(n-1)
-
+from .models import CSMInput
 
 def validator(input_data: Dict[str, Any]) -> CSMInput:
     """
